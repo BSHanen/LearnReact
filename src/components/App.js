558cadbf1262856bf0eb29f2b9c8b34f4ac81b72
BSHanen <hanenbendimerad@gmail.com>
2023-04-15 00:32:23 +0100

    Handler Events expanion

--- src/components/App.js
+++ src/components/App.js
@@ -6,14 +6,21 @@ class App extends Component {
         this.state = { number : 0}
         // pour chaque elements utiliser entre constructor et render il faut le declarer 
         // dans le consctuctor de cette maniere 
-        this.OnClickEvent = this.OnClickEvent.bind(this);
+        this.OnClickEventIncrement = this.OnClickEventIncrement.bind(this);
+        this.OnClickEventDesincrement = this.OnClickEventDesincrement.bind(this);
     }
-    OnClickEvent(){
+    OnClickEventIncrement(){
        this.setState(prevState => { // setState fonction predefinie dans la classe Components 
         // qui prends et  modifier la valeur du state declarer dans le Constructor directement et de facon gnrl
         // a l'aide de son parametre 'prevState' (Privious State)
         return { number : prevState.number + 1 } // entre {} car c du JS
        })
+    }
+    
+    OnClickEventDesincrement(){
+        this.setState(prevState => {
+             return { number : prevState.number - 1 }
+            })
     }
     render(){
 
@@ -22,7 +29,10 @@ class App extends Component {
                 <h1>{ this.state.number}</h1>
     
             <button  className = " rounded p-3 mb-2 bg-success text-white"
-             onClick={this.OnClickEvent}>Click Me to increment ! </button>
+             onClick={this.OnClickEventIncrement}>Click Me to increment ! </button>
+
+            <button  className = " rounded p-3 mb-2 bg-success text-white"
+             onClick={this.OnClickEventDesincrement}>Click Me to desincrement ! </button>
             </div>
         )
     }
